bc11bce96af7c60a83b1a2bca0b6a46f83bce29a
Soonho Kong <soonho.kong@tri.global>
2021-10-03 14:22:28 +0000

    Fix Clp include path

--- lp_lib_wrapper/clp/ibex_LPLibWrapper.h
+++ lp_lib_wrapper/clp/ibex_LPLibWrapper.h
@@ -1,7 +1,7 @@
 #ifndef _IBEX_LPLIBWRAPPER_H_
 #define _IBEX_LPLIBWRAPPER_H_
 
-#include "ClpSimplex.hpp"
+#include "coin/ClpSimplex.hpp"
 
 #define IBEX_LPSOLVER_WRAPPER_ATTRIBUTES ClpSimplex *myclp;\
                                          int * _col1Index
